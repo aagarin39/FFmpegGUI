44f448bcb74e97750bbbda54f92c8d658dc8371e
CodeFlow <a.a.garin39@gmail.com>
2026-04-05 19:10:40 +0300

    fix(build): fix macOS BUNDLE spec - EXE must wrap PYZ first

--- build.py
+++ build.py
@@ -208,13 +208,31 @@ a = Analysis(
 
 pyz = PYZ(a.pure, a.zipped_data, cipher=block_cipher)
 
-app = BUNDLE(
+exe = EXE(
     pyz,
     a.scripts,
     a.binaries,
     a.zipfiles,
     a.datas,
     [],
+    name='FFmpegConverter',
+    debug=False,
+    bootloader_ignore_signals=False,
+    strip=False,
+    upx=False,
+    upx_exclude=[],
+    runtime_tmpdir=None,
+    console=False,
+    disable_windowed_traceback=False,
+    argv_emulation=False,
+    target_arch=None,
+    codesign_identity=None,
+    entitlements_file=None,
+    icon='{icon}',
+)
+
+app = BUNDLE(
+    exe,
     name='FFmpegConverter.app',
     icon='{icon}',
     bundle_identifier='com.aagarin39.ffmpegconverter',
